59b434520497569c1436b5b351942aadc40b901a
Sam <sam@sammcnab.co.uk>
2024-09-16 12:04:36 +0100

    feat: add boilerplate for portfolio

--- src/common/constant/learn.ts
+++ src/common/constant/learn.ts
@@ -5,22 +5,11 @@ export const LEARN_CONTENTS: ContentProps[] = [
     id: 1,
     title: 'Association for Learning Technology (ALT) Portfolio',
     slug: 'cmalt-portfolio',
-    description: 'Learn more about my work surrounding Learning Technology and its implementation.',
-    image: '/public/learn/ALT.png',
-    is_new: true,
-    level: 'Beginner',
-    is_show: true,
-  },
-  {
-    id: 2,
-    title: 'Problem Solving',
-    slug: 'problem-solving',
     description:
-      'Learn problem solving in JavaScript with detailed explanations.',
-    image:
-      '',
-    is_new: false,
-    level: 'All Levels',
-    is_show: false,
+      'Learn more about my work surrounding Learning Technology and its implementation.',
+    image: '/images/learn/ALT.png',
+    is_new: true,
+    level: 'Advanced',
+    is_show: true,
   },
 ];
